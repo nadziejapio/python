4a638a9177475689cef9bd3feafbad300c1fbe9e
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-08-10 20:19:13 +0000

    ddd, Aug 8/10/2022, YYYY, 8:19 A +0

--- migration/pset6/dna/dna/dna.py
+++ migration/pset6/dna/dna/dna.py
@@ -5,7 +5,9 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-
+    if len(sys.argv) != 3:
+        print ("Usage: python dna.py data.csv sequence.txt")
+        sys.exit(1)
     # TODO: Read database file into a variable
 
     # TODO: Read DNA sequence file into a variable
