fd4eaf93efbd816dbe6868f7657cdbb1fbb2702c
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-08-11 11:10:37 +0000

    ddd, Aug 8/11/2022, YYYY, 11:10 A +0

--- migration/pset6/dna/dna/dna.py
+++ migration/pset6/dna/dna/dna.py
@@ -10,7 +10,7 @@ def main():
         sys.exit(1)
     # TODO: Read database file into a variable
     with open(sys.argv[1], 'r') as csvfile:
-    csvreader = csv.DictReader(csvfile)
+        csvreader = csv.DictReader(csvfile)
     print(csvfile)
     # TODO: Read DNA sequence file into a variable
 
