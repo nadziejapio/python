43dea3c345ed937861b93c972b036e1ed89a4869
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-08-29 21:16:51 +0000

    ddd, Aug 8/29/2022, YYYY, 9:16 A +0

--- migration/movies/7.sql
+++ migration/movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies JOIN ratings ON ratings.movie_id = movies.id WHERE year = 2010;
+SELECT title, rating FROM movies, ratings WHERE ratings.movie_id = movies.id AND year = 2010;
