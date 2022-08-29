edfe1c1ad8e497afbee425e6165508ab28a0180b
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-08-29 21:48:51 +0000

    ddd, Aug 8/29/2022, YYYY, 9:48 A +0

--- migration/fiftyville/log.sql
+++ migration/fiftyville/log.sql
@@ -2,3 +2,10 @@
 -- Check what is in Crime reports
 SELECT *
 FROM crime_scene_reports;
+-- check what is in crime reports on July 28, 2021 on Humphrey Street
+SELECT *
+FROM crime_scene_reports
+WHERE day = 28
+AND month = 7
+AND year = 2021
+AND street = "Humphrey Street";
