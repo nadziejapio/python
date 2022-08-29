b566f4bc4131d4fd43553729c93c1fdb5669b46c
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-08-29 21:08:45 +0000

    ddd, Aug 8/29/2022, YYYY, 9:08 A +0

---
+++ migration/movies/13.sql
@@ -0,0 +1,1 @@
+SELECT name FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Johnny Depp" AND title IN (SELECT title FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Helena Bonham Carter");
