961d44fe819f32e6aab5ea52f82f6352f4c154f5
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-08-29 21:12:42 +0000

    ddd, Aug 8/29/2022, YYYY, 9:12 A +0

--- migration/movies/13.sql
+++ migration/movies/13.sql
@@ -1,1 +1,1 @@
-SELECT name FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND title IN (SELECT title FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Kevin Bacon" AND birth = 1958);
+SELECT name FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND title IN (SELECT title FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Kevin Bacon" AND birth = 1958) NOT "Kevin Bacon";
