f4ff71198327b4f119459698509c1e54627b9f36
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-08-29 21:10:53 +0000

    ddd, Aug 8/29/2022, YYYY, 9:10 A +0

--- migration/movies/13.sql
+++ migration/movies/13.sql
@@ -1,1 +1,1 @@
-SELECT name FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Johnny Depp" AND title IN (SELECT title FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Helena Bonham Carter");
+SELECT name FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND title IN (SELECT title FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND name = "Kevin Bacon" AND birth = 1958);
