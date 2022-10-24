cb557ba35bc7ce1ff30c228b4bcfd505119ff2b0
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-10-24 20:45:06 +0000

    Mon, Oct 24, 2022, 8:45 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -129,7 +129,7 @@ def register():
             return apology("user already exists", 403)
         if request.form.get("password") != request.form.get("confirmation"):
             return apology("passwords don't match", 403)
-        db.execute("INSERT )
+        db.execute("INSERT INTO users )
 
     else:
         return render_template("register.html")
