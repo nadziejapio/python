a9db22699000213d953059ae6a879238f9d706c1
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-11-05 21:48:56 +0000

    Sat, Nov 5, 2022, 9:48 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -44,7 +44,7 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    stocks = db.execute("SELECT symbol FROM users, transactions WHERE username = ?")
+    stocks = db.execute("SELECT symbol, COUNT(id), username FROM transactions GROUP BY symbol WHERE username = ?")
     return render_template("index.html", stocks=stocks, symbol=symbol, number=number, price=price, total=total, bank=bank )
 
 
