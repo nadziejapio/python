de7988822c1771cea7a29f4255b1746f35098027
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-11-06 00:30:20 +0000

    Sun, Nov 6, 2022, 12:30 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -47,7 +47,7 @@ def index():
     stocks = db.execute("SELECT symbol, SUM(number), username, nazwa FROM transactions WHERE username = ? GROUP BY symbol, nazwa", db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])[0].get("username"))
     for stock in stocks:
         stock.update({"pricenow": lookup(stock['symbol'])['price']})
-    bank = db.execute(")
+    bank = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0].get("cash")
     return render_template("index.html", stocks=stocks, bank=bank)
 
 
