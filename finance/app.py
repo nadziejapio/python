2c07f1dcb976bb112d4ab6bfca2b4a3434248db8
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-11-08 20:57:47 +0000

    Tue, Nov 8, 2022, 8:57 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -200,4 +200,4 @@ def sell():
         return redirect("/")
     else:
         stocks = db.execute("SELECT symbol, SUM(number), username, nazwa FROM transactions WHERE username = ? GROUP BY symbol, nazwa", db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])[0].get("username"))
-        return render_template("sell.html")
+        return render_template("sell.html", stocks=stocks)
