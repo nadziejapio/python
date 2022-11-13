31ea93716252a90db10de5709bd9a9c1c6e0ecf5
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-11-13 22:34:27 +0000

    Sun, Nov 13, 2022, 10:34 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -196,13 +196,13 @@ def sell():
     if request.method == "POST":
         name = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])[0].get("username")
         if request.form.get("symbol") == "":
-            return apology("wrong symbol", 403)
+            return apology("wrong symbol", 400)
         elif int(request.form.get("shares")) > int(db.execute("SELECT SUM(number) FROM transactions WHERE username = ? AND symbol = ? GROUP BY symbol", name, request.form.get("symbol"))[0].get('SUM(number)')):
-            return apology("not enough shares", 403)
+            return apology("not enough shares", 400)
         elif int(request.form.get("shares")) <= 0:
-            return apology("must be positive number", 403)
+            return apology("must be positive number", 400)
         elif request.form.get("symbol") not in db.execute("SELECT symbol FROM transactions WHERE username = ? AND symbol = ? GROUP BY symbol", name, request.form.get("symbol"))[0]['symbol']:
-            return apology("must be in your wallet", 403)
+            return apology("must be in your wallet", 400)
 
         print (name)
         sym = request.form.get("symbol")
