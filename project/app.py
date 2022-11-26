fc314343ef6a4a071b819f141118e2eadfdd13da
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-11-26 21:29:51 +0000

    Sat, Nov 26, 2022, 9:29 PM +00:00

--- project/app.py
+++ project/app.py
@@ -5,8 +5,6 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import datetime
 
 from helpers import apology, login_required, lookup, usd
 
