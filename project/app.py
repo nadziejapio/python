18ae5c64cd90d8ccb8bef7bc438683dd3f5cc154
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-12-08 21:11:12 +0000

    Thu, Dec 8, 2022, 9:11 PM +00:00

--- project/app.py
+++ project/app.py
@@ -5,6 +5,8 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 from helpers import apology, login_required, lookup, usd
 
@@ -13,6 +15,9 @@ app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
