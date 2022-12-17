931c03c9b076defd46a0d10db1d92f89b56d69c0
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-12-17 08:20:17 +0000

    Sat, Dec 17, 2022, 8:20 AM +00:00

--- project/app.py
+++ project/app.py
@@ -8,7 +8,7 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
-from helpers import apology, login_required
+from helpers import login_required
 
 # Configure application
 app = Flask(__name__)
--- project/helpers.py
+++ project/helpers.py
@@ -8,3 +8,4 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+    
