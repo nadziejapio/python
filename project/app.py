61c13ee04725a2c3ecb42d5764054ca1a11f8b81
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-12-17 09:54:59 +0000

    Sat, Dec 17, 2022, 9:54 AM +00:00

--- project/app.py
+++ project/app.py
@@ -87,12 +87,7 @@ def history():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
-
-    # Forget any user_id
     session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
         # Ensure username was submitted
