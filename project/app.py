3ac90985ac39954415c189f8bed406d6364c407e
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-12-17 16:17:46 +0000

    Sat, Dec 17, 2022, 4:17 PM +00:00

--- project/app.py
+++ project/app.py
@@ -34,7 +34,8 @@ def index():
     books = db.execute("SELECT * FROM book WHERE ownersID = ?", session["user_id"])
     for book in books:
         if book["readerID"] != None:
-            book.update(db.execute("SELECT * FROM person WHERE person_id = ?", book['readerID']))
+            guy = db.execute("SELECT * FROM person WHERE person_id = ?", book['readerID'])
+            book.update(guy[0])
     print(books)
     return render_template("index.html", books=books)
 
