3ec81f8308822f4de5148b3e73d83e1c8050bb17
nadziejapio <84388562+nadziejapio@users.noreply.github.com>
2022-12-17 12:38:19 +0000

    Sat, Dec 17, 2022, 12:38 PM +00:00

--- project/app.py
+++ project/app.py
@@ -32,7 +32,7 @@ def after_request(response):
 @login_required
 def index():
     books = db.execute(
-        "SELECT title, status, name, surname, nick, time FROM book, person, users WHERE username = ?, ownerID = ? ", db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])[0].get("username"), db.execute("SELECT username FROM users WHERE id = ?", session["user_id"]))
+        "SELECT title, status, name, surname, nick, time FROM book, person, users WHERE username = ?, ownerID = ? ", (db.execute("SELECT username FROM users WHERE id = ?", session["user_id"]))[0].get("username"), db.execute("SELECT username FROM users WHERE id = ?", session["user_id"]))
     print(books)
     return render_template("index.html", books=books)
 
