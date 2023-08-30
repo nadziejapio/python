dc77cd276d775eb7313a7c8cd8124da268997d60
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-30 11:24:33 +0000

    Wed, Aug 30, 2023, 1:24 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,16 +1,16 @@
 i = input("File name: ")
 match i:
-    case i.endswith(.gif):
-        print("")
-    case i.endswith(.jpg):
-        print("")
-    case i.endswith(.jpeg):
-        print("")
-    case i.endswith(.png):
-        print("")
-    case i.endswith(.pdf):
-        print("")
-    case i.endswith(.txt):
-        print("")
-    case i.endswith(.zip):
-        print("")
+    case i.endswith(".gif"):
+        print("image/gif")
+    case i.endswith(".jpg") | i.endswith(".jpeg"):
+        print("image/jpeg")
+    case i.endswith(".png"):
+        print("image/png")
+    case i.endswith(".pdf"):
+        print("application/pdf")
+    case i.endswith(".txt"):
+        print("text/plain")
+    case i.endswith(".zip"):
+        print("application/zip")
+    case _:
+        print("application/octet-stream")
