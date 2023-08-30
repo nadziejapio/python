2af781446c0cbac32f31cbfef34523187c8440c7
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-30 11:26:13 +0000

    Wed, Aug 30, 2023, 1:26 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,11 +1,10 @@
 i = input("File name: ")
-match i:
-    case i.endswith(".gif"):
-        print("image/gif")
-    case i.endswith(".jpg") | i.endswith(".jpeg"):
-        print("image/jpeg")
-    case i.endswith(".png"):
-        print("image/png")
+if i.endswith(".gif"):
+    print("image/gif")
+elif i.endswith(".jpg") or i.endswith(".jpeg"):
+    print("image/jpeg")
+case i.endswith(".png"):
+print("image/png")
     case i.endswith(".pdf"):
         print("application/pdf")
     case i.endswith(".txt"):
