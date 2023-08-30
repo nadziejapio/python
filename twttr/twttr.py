98447655e24ebd3d095adba49f9ccc8315b68691
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-30 16:48:08 +0000

    Wed, Aug 30, 2023, 6:48 PM +02:00

---
+++ twttr/twttr.py
@@ -0,0 +1,8 @@
+i = input("Input: ")
+print("Output: ", end="")
+for letter in i:
+    if letter in ["a","A","e","E","i","I","o","O","u","U"]:
+        continue
+    else:
+        print(letter, end="")
+print()
