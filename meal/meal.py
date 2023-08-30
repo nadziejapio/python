fc253391b5c5accfc7a0d22b578490653a927af4
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-30 12:28:58 +0000

    Wed, Aug 30, 2023, 2:28 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,7 +1,14 @@
 def main():
-    i = input("What time is it? ")
+    i = convert(input("What time is it? "))
+    if 7 <= i <= 8:
+        print("breakfest time")
+    elif 12 <= i <= 13:
+        print("lunch time")
+    elif 18 <= i <= 19:
+        print("dinner time")
 
 def convert(time):
-    
+    h, m = int(time.split(":"))
+    return h + (m/60)
 
 main()
