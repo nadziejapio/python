47d6675506d15b9b55c2eca2afd0b1fe3fe8b7cb
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-30 11:39:42 +0000

    Wed, Aug 30, 2023, 1:39 PM +02:00

---
+++ interpreter/interpreter.py
@@ -0,0 +1,11 @@
+x,y,z = input("Expression: ").split(" ")
+x = int(x)
+z = int(z)
+if y == "+":
+    print({x + y})
+elif y == "-":
+    print({x - z})
+elif y == "*":
+    print({x * z})
+elif y == "/":
+    print({x/z})
