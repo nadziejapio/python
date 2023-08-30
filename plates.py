2f096f15b4f0bc56614b0f4391da74e6aeb02b20
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-30 17:04:27 +0000

    Wed, Aug 30, 2023, 7:04 PM +02:00

--- plates.py
+++ plates.py
@@ -7,7 +7,18 @@ def main():
 
 
 def is_valid(s):
-    ...
+    if start_with_two_letters(s) and min_max_characters(s) and numbers(s) and signs(s):
+        return True
+    else
+        return False
 
+def start_with_two_letters(plate):
+
+def min_max_characters(plate):
+
+def numbers(plate):
+
+def signs(plate):
+    
 
 main()
