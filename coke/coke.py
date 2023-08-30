2ef3bb0d815bebec298302f1838e6121d0d6ab35
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-30 16:36:33 +0000

    Wed, Aug 30, 2023, 6:36 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,10 +1,10 @@
 a = 50
 while True:
     if a > 0:
-        print ("Amount Due: ", a)
+        print ("Amount Due:", a)
         i = int(input("Insert Coin: "))
         if i == 25 or i == 10 or i == 5:
             a -= i
     else:
-        print("Change Owed: ", a)
+        print("Change Owed:", -a)
         break
