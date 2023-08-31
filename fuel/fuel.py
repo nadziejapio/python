c85c410838b2a7702a5493ff84a0fe31fa784dda
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-31 12:49:30 +0000

    Thu, Aug 31, 2023, 2:49 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,7 +2,12 @@ while True:
     try:
         x,y = input("Fraction: ").split("/")
         ans = int(x)/int(y)*100
-        print(f"{int(ans)}%")
+        if ans > 99:
+            print("F")
+        elif ans < 1:
+            print ("E")
+        else:
+            print(f"{int(ans)}%")
         break
     except (ValueError, ZeroDivisionError):
         continue
