5f719139563caac6024031718c57303300b986f4
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-31 12:55:04 +0000

    Thu, Aug 31, 2023, 2:55 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,12 +2,12 @@ while True:
     try:
         x,y = input("Fraction: ").split("/")
         ans = int(x)/int(y)*100
-        if ans > 98:
-            print("F")
+        if ans > 100:
+            continue
         elif ans < 2:
             print ("E")
-        elif ans > 100:
-            continue
+        elif ans > 98:
+            print("F")
         else:
             print(f"{round(ans)}%")
         break
