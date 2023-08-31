3688d46d99ed113743ecfff93b98ae086b0b0d8d
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-08-31 15:29:55 +0000

    Thu, Aug 31, 2023, 5:29 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,10 +1,12 @@
 grocery_list = {}
 while True:
     try:
-        i = input().capitalize()
+        i = input().upper()
         if i in grocery_list:
             grocery_list[i] += 1
         else:
             grocery_list[i] = 1
     except EOFError:
-        print(grocery_list)
+        for j in grocery_list:
+            print(grocery_list.get(j), j)
+        break
