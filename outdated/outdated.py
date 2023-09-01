07ea85daaaefe09098c3d414d3e77ee38ab64c93
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-01 11:00:54 +0000

    Fri, Sep 1, 2023, 1:00 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -14,14 +14,13 @@ months = [
 ]
 
 while True:
-    try:
+    #try:
         i = input("Date: ")
         print(i, i[0])
         if i[0].isalpha:
             m, d, y = i.split()
         else:
             m, d, y = i.split("/")
-
         print(m, d, y, sep=", ")
         if int(d) in range(1,32):
             if m in months:
@@ -34,5 +33,5 @@ while True:
                 continue
         else:
             continue
-    except Exception:
-       continue
+    #except Exception:
+    #  continue
