ec5da951dcf797dbb06604fac61716df6d0dbecf
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-01 14:19:56 +0000

    Fri, Sep 1, 2023, 4:19 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -19,8 +19,11 @@ while True:
         if i[0].isalpha():
             m, d, y = i.split()
         else:
-            m, d, y = i.split("/")
-        d = d.strip(',')
+            m, d, y = i.split("/").strip
+        if d[-1]:
+            d = d.strip(',')
+        else:
+            continue
         m = m.strip()
         if int(d) in range(1,32):
             if m in months:
