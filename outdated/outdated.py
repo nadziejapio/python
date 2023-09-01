5252741cc39cd3a49e176d9ce545a458cf319bed
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-01 06:51:41 +0000

    Fri, Sep 1, 2023, 8:51 AM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -16,4 +16,14 @@ months = [
 while True:
     try:
         m, d, y = input("Date: ").split("/")
-        if
+        if d in range(1,32):
+            if m in months:
+                print(f"{y}-{m}-{d:02}")
+            elif int(m) in range(1, 13):
+                print(f"{y}-{m:02}-{d:02}")
+            else:
+                continue
+        else:
+            continue
+    except exception:
+        continue
