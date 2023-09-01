69e367e77181b0795b7b58911bf86f0fd8488caf
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-01 06:30:51 +0000

    Fri, Sep 1, 2023, 8:30 AM +02:00

---
+++ outdated/outdated.py
@@ -0,0 +1,19 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+while True:
+    try:
+        m, d, y = input("Date: ").split("/")
+        if
