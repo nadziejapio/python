269f48592c743731c28ad19ea610e405acb65e75
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-01 18:07:29 +0000

    Fri, Sep 1, 2023, 8:07 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,3 +1,12 @@
 from pyfiglet import Figlet
 import sys
 
+figlet = Figlet()
+fonts = figlet.getFonts()
+
+if sys.argv == 0:
+    print(fonts)
+elif sys.argv == 2:
+    sys.exit()
+else:
+    sys.exit()
