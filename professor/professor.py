561052ec86828e43680b1812f026de7c559657c1
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-04 09:32:56 +0000

    Mon, Sep 4, 2023, 11:32 AM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -8,9 +8,12 @@ def get_level():
     while True:
         try:
             i = int(input("Level: "))
-        if i
-    i
-
+            if i in [1,2,3]:
+                return i
+            else:
+                continue
+        except Exceptation:
+            continue
 
 def generate_integer(level):
     i = random.randrange(10^(level-1), 10^level)
