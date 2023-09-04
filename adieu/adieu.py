7c461aae78e81b8514ac7a352dfed45f612f4280
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-04 07:35:22 +0000

    Mon, Sep 4, 2023, 9:35 AM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,3 +1,10 @@
+import inflect
+p = inflect.engine()
+
 names = []
 while True:
-    i = input("Name: ")
+    try:
+        i = input("Name: ")
+        names.append(i)
+    except EOFError:
+        print(p.join((name for name in names), final_sep=""))
