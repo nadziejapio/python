4fc278b92da259ba490d4b65eab7d04a508c997a
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-04 08:56:34 +0000

    Mon, Sep 4, 2023, 10:56 AM +02:00

---
+++ professor/professor.py
@@ -0,0 +1,16 @@
+import random
+
+def main():
+    while True:
+        i = input("")
+
+def get_level():
+    ...
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
