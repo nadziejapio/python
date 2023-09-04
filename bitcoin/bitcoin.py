c33436ceecf64a3cc5f7ae9cd6949b2116175cd6
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-04 13:01:04 +0000

    Mon, Sep 4, 2023, 3:01 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -7,7 +7,7 @@ try:
         sys.exit("Missing command-line argument")
     n = float(sys.argv[1])
     response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    print(response.json())
+    print(json.dumps(response.json(), indent=2))
 
 except requests.RequestException:
     pass
