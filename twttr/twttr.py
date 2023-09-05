402a1d16f6dfd89121242a4765ea5cf46e5a8eed
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-05 13:48:56 +0000

    Tue, Sep 5, 2023, 3:48 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,5 +1,6 @@
-i = input("Input: ")
-print("Output: ", end="")
+def main():
+    i = input("Input: ")
+    print("Output: ", end="")
 for letter in i:
     if letter in ["a","A","e","E","i","I","o","O","u","U"]:
         continue
