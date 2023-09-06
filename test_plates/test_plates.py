e34d961e685c17d724344c6a829101bf27e98f90
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-06 09:05:23 +0000

    Wed, Sep 6, 2023, 11:05 AM +02:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -12,6 +12,7 @@ def test_is_valid_min_max():
 def test_is_valid_numbers():
     assert is_valid("AAA22A") == False
     assert is_valid("AAA222") == True
+    assert is_valid("AAAB02") == False
 
 def test_is_valid_puntuation():
     assert is_valid("AA,,22") == False
