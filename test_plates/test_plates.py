7a63550791feb0bd9a48ef4033460848c4902e95
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-06 08:55:40 +0000

    Wed, Sep 6, 2023, 10:55 AM +02:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,12 +1,13 @@
 from plates import is_valid
 
 def test_is_valid_two_letters():
-    assert is_valid("AAbcd1") == True
+    assert is_valid("-/dsvc")
     assert is_valid("12abcd") == False
 
 def test_is_valid_min_max():
     assert is_valid("A") == False
     assert is_valid("ABDCSD2") == False
+    assert is_valid("") == False
 
 def test_is_valid_numbers():
     assert is_valid("AAA22A") == False
