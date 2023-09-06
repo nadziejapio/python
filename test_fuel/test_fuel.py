b2cfade682a6d7ece3d58fc9cf89decbaf7bee43
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-06 11:51:41 +0000

    Wed, Sep 6, 2023, 1:51 PM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,11 +1,14 @@
 import fuel
 
 def test_convert:
-    assert fuel.convert() ==
+    assert fuel.convert("3/6") == 50
+    with pytest.raises(ValueError):
+        fuel.convert("5/4")
+    with pytest.raises(ZeroDivisionError):
+        fuel.convert("2/0")
 
 def test_gauge:
     assert fuel.gauge(0.5) == "E"
     assert fuel.gauge(1) == "E"
     assert fuel.gauge(99) == "F"
     assert fuel.gauge(100) =="F"
-    with pytest.raises(ValueError)
