ed65949dd902d9ba4842681a701e1563477c5823
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-06 08:27:59 +0000

    Wed, Sep 6, 2023, 10:27 AM +02:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -2,9 +2,11 @@ from bank import value
 
 def test_value_0():
     assert value("hello my friend") == 0
+    assert value("Hello") == 0
 
 def test_value_20():
     assert value("hi my friend") == 20
+    assert value("Hi") == 20
 
 def test_vaalue_100():
     assert value("good morning") == 100
