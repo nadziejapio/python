22b8f05073f56346def21d5b80238f62e7f1d8f6
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-06 07:58:01 +0000

    Wed, Sep 6, 2023, 9:58 AM +02:00

--- twttr/test_twttr.py
+++ twttr/test_twttr.py
@@ -1,7 +1,7 @@
 from twttr import shorten
 
 def test_shorten():
-    assert shorten("Hello") == "Hll"
+    assert shorten("HellO") == "Hll"
     assert shorten("o") == ""
     assert shorten("123") == "123"
     assert shorten("cd.fg") == "cd.fg"
