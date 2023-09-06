3b689cd3802391b4fd88bfaa629ed9042be88915
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-06 08:35:20 +0000

    Wed, Sep 6, 2023, 10:35 AM +02:00

---
+++ test_plates/plates.py
@@ -0,0 +1,39 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if start_with_two_letters(s) and numbers(s) and signs(s):
+        return True
+    else:
+        return False
+
+def start_with_two_letters(plate):
+    if 1 < len(plate) < 7:
+        if plate[0:1].isalpha():
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def numbers(plate):
+    for i in range(len(plate)):
+        if plate[i].isdigit():
+            if plate[i] == "0":
+                return False
+            else:
+                return plate[i:len(plate)].isdigit()
+    return True
+
+def signs(plate):
+    if plate.isalnum():
+        return True
+    else:
+        return False
+
+main()
