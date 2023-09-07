d5c81ae7d1831d39474bfcf34009b7f3cd985c84
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-07 07:53:01 +0000

    Thu, Sep 7, 2023, 9:53 AM +02:00

---
+++ lines/lines.py
@@ -0,0 +1,9 @@
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-lines arguments")
+else:
+    if sys.argv[1]
+i = sys.argv
