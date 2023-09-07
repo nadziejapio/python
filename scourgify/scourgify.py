c24b9cac3520a7d9e546151652a19731bf732a42
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-07 12:01:43 +0000

    Thu, Sep 7, 2023, 2:01 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -7,10 +7,12 @@ elif len(sys.argv) > 3:
     sys.exit("Too many command-lines arguments")
 else:
     try:
-        students = {}
+        students = []
         with open('before.csv') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 students.append(row)
+
+            print(students)
     except FileNotFoundError:
         sys.exit(f'Could not read {sys.argv[1]}')
