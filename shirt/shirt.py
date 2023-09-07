ad56fddaa2b94b6ffb2da643622b5602c21156e3
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-07 15:29:36 +0000

    Thu, Sep 7, 2023, 5:29 PM +02:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -7,4 +7,5 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-lines arguments")
-elif sys.argv[1]
+elif os.path.splitext(sys.argv[1])[1].lower() not in [".jpg", ".jpeg", ".png"] or os.path.splitext(sys.argv[2])[1].lower() not in [".jpg", ".jpeg", ".png"]:
+    sys.exit(")
