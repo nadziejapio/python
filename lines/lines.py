633a28ebf7ae0eb61de398b831e903735e3082b9
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-07 07:58:52 +0000

    Thu, Sep 7, 2023, 9:58 AM +02:00

--- lines/lines.py
+++ lines/lines.py
@@ -4,7 +4,12 @@ if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-lines arguments")
+elif not sys.argv[1].endswith(".py"):
+    sys.exit("Not a Python file")
 else:
-    if not sys.argv[1].endswith(".py"):
-        sys.exit("Not a Python file")
-i = sys.argv
+    try:
+        with open(sys.argv[1]) as file:
+            for line in file:
+                print(line)
+    except FileNotFoundError:
+        sys.exit("File not found")
