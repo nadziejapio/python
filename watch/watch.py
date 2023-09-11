22533a2a722709f6184e7728cc1ad231844769f1
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-11 12:37:12 +0000

    Mon, Sep 11, 2023, 2:37 PM +02:00

--- watch/watch.py
+++ watch/watch.py
@@ -5,7 +5,7 @@ def main():
     print(parse(input("HTML: ")))
 
 def parse(s):
-    if matches := re.search(r"^.+\"https?://(?:www\.)?youtube\.com./embed/(.+)\".+$", s):
+    if matches := re.search(r"^<iframe.+\"https?://(?:www\.)?youtube\.com./embed/(.+)\".+$", s):
         return "ok
     #f"https://youtu.be/{matches.group(1)}"
 
