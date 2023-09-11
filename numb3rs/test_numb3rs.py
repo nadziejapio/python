07b7af2faa9fc786be6eaaf5a1e7a4245d9c9112
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-11 08:57:11 +0000

    Mon, Sep 11, 2023, 10:57 AM +02:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -3,6 +3,7 @@ from numb3rs import validate
 def test_numb3rs_nums():
     assert validate("255.255.255.0") == True
     assert validate("275.0.3.68") == False
+    assert validate("220") == False
 
 def test_numb3rs_words():
     assert validate("cat") == False
