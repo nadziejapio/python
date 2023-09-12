fddd8e27b0403507306853e329f03c581b28ccf4
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-12 05:38:10 +0000

    Tue, Sep 12, 2023, 7:38 AM +02:00

--- watch/watch.py
+++ watch/watch.py
@@ -5,7 +5,7 @@ def main():
     print(parse(input("HTML: ")))
 
 def parse(s):
-    if matches := re.search(r'.+https?://(?:www\.)?youtube\.com/embed/(.+)" (.+)', s):
+    if matches := re.search(r'.+https?://(?:www\.)?youtube\.com/embed/([^\"]+)\" (.+)', s):
         return f"https://youtu.be/{matches.group(1)}"
 
 if __name__ == "__main__":
