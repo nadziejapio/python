6cdf134fed287181d8c4c01efd8c4dea44d9379f
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-12 13:31:31 +0000

    Tue, Sep 12, 2023, 3:31 PM +02:00

--- um/test_um.py
+++ um/test_um.py
@@ -1,5 +1,10 @@
 from um import count
 
 def test_word():
-    assert count("sodium") == "0"
+    assert count("sodium") == 0
 
+def test_space():
+    assert count(" Um.") == 1
+
+def test_nospace():
+    assert count(",um,") == 1
