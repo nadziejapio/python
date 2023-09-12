aaa283b440ec62f43b5f9b10aac9bef3cfdee9e1
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-12 12:03:11 +0000

    Tue, Sep 12, 2023, 2:03 PM +02:00

--- working/working.py
+++ working/working.py
@@ -9,11 +9,11 @@ def main():
 def convert(s):
     if matches:= re.search(r"(\d\d?)(?::(\d\d))? [AP]M to (\d\d?)(?::(\d\d))? [AP]M", s):
          ans = matches.groups()
-    #h1, m1, h2, m2
-    #    if not 0 <= int(m1) < 60:
-    #        raise ValueError
-    #    if not 0 <= int(m2) < 60:
-    #        raise ValueError
+    h1, m1, h2, m2
+        if not 0 <= int(m1) < 60:
+            raise ValueError
+        if not 0 <= int(m2) < 60:
+            raise ValueError
 
          return ans
 
