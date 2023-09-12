980b792679d899f6db2f9dcf77113d7c679602d4
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-12 09:07:04 +0000

    Tue, Sep 12, 2023, 11:07 AM +02:00

--- working/working.py
+++ working/working.py
@@ -9,6 +9,11 @@ def main():
 def convert(s):
     if matches:= re.search(r"(\d\d?):(\d\d) [AP]M to (\d\d?):(\d\d) [AP]M", s):
         h1, m1, h2, m2 = matches.groups()
+        if not 0 <= int(m1) < 60:
+            raise ValueError
+        if not 0 <= int(m2) < 60:
+            raise ValueError
+
         return h1, m1, h2, m2
 
 if __name__ == "__main__":
