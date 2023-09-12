8e8803d95d26cf63cc2c0e9cabcf42693279b3dd
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-12 13:03:01 +0000

    Tue, Sep 12, 2023, 3:03 PM +02:00

--- working/working.py
+++ working/working.py
@@ -19,17 +19,25 @@ def convert(s):
                 raise ValueError
         else:
             m2 = "00"
-
-        if a1 == "PM":
+        if a1 == "AM":
             if h1 == "12":
                 h1 = 00
+        if a2 == "AM":
+            if h2 == "12":
+                h2 = 00
+        if a1 == "PM":
+            if h1 == "12":
+                h1 = 12
             else:
                 h1 = int(h1) + 12
         if a2 == "PM":
-            if h2 ==
-            h2 = int(h2) + 12
+            if h2 == "12":
+                h2 = 12
+            else:
+                h2 = int(h2) + 12
 
         return f"{int(h1):02}:{m1} to {int(h2):02}:{m2}"
-
+    else:
+        raise ValueError
 if __name__ == "__main__":
     main()
