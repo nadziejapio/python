9dea94e9e7b1df0def6555e498c6d44b5e3f4da1
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-12 12:09:30 +0000

    Tue, Sep 12, 2023, 2:09 PM +02:00

--- working/working.py
+++ working/working.py
@@ -9,10 +9,16 @@ def main():
 def convert(s):
     if matches:= re.search(r"(\d\d?)(?::(\d\d))? [AP]M to (\d\d?)(?::(\d\d))? [AP]M", s):
         h1, m1, h2, m2 = matches.groups()
-        if m1 and not 0 <= int(m1) < 60:
-            raise ValueError
-        if m2 and not 0 <= int(m2) < 60:
-            raise ValueError
+        if m1:
+            if not 0 <= int(m1) < 60:
+                raise ValueError
+            else:
+                m1 = "00"
+        if m2:
+            if not 0 <= int(m2) < 60:
+                raise ValueError
+            else:
+                m2 = "00"
         return h1, m1, h2, m2
 
 if __name__ == "__main__":
