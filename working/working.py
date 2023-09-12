383be222cd854940eacfe884a44f9759184b7b15
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-12 09:02:04 +0000

    Tue, Sep 12, 2023, 11:02 AM +02:00

--- working/working.py
+++ working/working.py
@@ -7,7 +7,9 @@ def main():
 
 
 def convert(s):
-    matches = re.search(r"", s)
+    if matches:= re.search(r"(/d/d?):(/d/d) [AP]M to (/d/d?):(/d/d) [AP]M", s):
+        h1, m1, h2, m2 = matches.groups()
+        return h1, m1, h2, m2
 
 if __name__ == "__main__":
     main()
