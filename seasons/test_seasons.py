dcf9b6939b870f571d019dc8235f1dda0f6c1279
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-09-23 18:11:55 +0000

    Sat, Sep 23, 2023, 8:11 PM +02:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,7 +1,8 @@
 from seasons import get_date
 import pytest
+import datetime
 
 def test_get_date():
-    assert get_date("1993-01-04") == "1993-01-04"
+    assert get_date("1993-01-04") == datetime.date(1993,1,4)
     with pytest.raises(ValueError):
         get_date("January 4, 1993")
