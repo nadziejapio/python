d8f898829d7b69c756c437e637b881bfe091180a
Pio <84388562+nadziejapio@users.noreply.github.com>
2023-10-04 06:51:43 +0000

    Wed, Oct 4, 2023, 8:51 AM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,22 +3,17 @@ from fpdf import FPDF
 
 class PDF(FPDF):
     def header(self):
-        # Setting font: helvetica bold 15
-        self.set_font("helvetica", "B", 15)
-        # Moving cursor to the right:
-        self.cell(80)
-        # Printing title:
-        self.cell(30, 10, input("Name:"), align="C")
-        # Performing a line break:
-        self.ln(20)
+        self.set_font("helvetica", "B", 48)
+        self.cell(180, 50, "CS50 Shirtificate", align="C")
+        self.ln(60)
 
 
-# Instantiation of inherited class
 pdf = PDF()
 pdf.add_page()
-pdf.set_font("Times", size=12)
+pdf.set_margin(0)
+pdf.set_auto_page_break(0)
+pdf.set_font(size=24)
 pdf.image("shirtificate.png", keep_aspect_ratio=True, )
-pdf.set_page_background('shirtificate.png')
-for i in range(1, 41):
-    pdf.cell(0, 10, f"Printing line number {i}", new_x="LMARGIN", new_y="NEXT")
-pdf.output("new-tuto2.pdf")
+pdf.set_text_color(255,255,255)
+pdf.cell(0, -260, input("Name: ") + " took CS50", align="C")
+pdf.output("shirtificate.pdf")
