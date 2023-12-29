ed711ffe69bd9e6cdbffa0d6dad15cdc2738b275
Piotr Górny <84388562+nadziejapio@users.noreply.github.com>
2023-12-29 10:04:40 +0000

    Fri, Dec 29, 2023, 11:04 AM +01:00

--- final/test_project.py
+++ final/test_project.py
@@ -5,7 +5,7 @@ def test_load_player():
     assert project.load_player('Nadzieja') == 'something is wrong'
 
 def test_login_to_facebook():
-
+    assert project.login_to_facebook()
 
 def test_check_player_status():
     assert project.check_player_status() == 'checked'
