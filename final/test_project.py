8f6302f600e0a7c3e91beaf2f1c60a5a5c62a619
Piotr Górny <84388562+nadziejapio@users.noreply.github.com>
2023-12-29 10:08:28 +0000

    Fri, Dec 29, 2023, 11:08 AM +01:00

--- final/test_project.py
+++ final/test_project.py
@@ -5,7 +5,9 @@ def test_load_player():
     assert project.load_player('Nadzieja') == 'something is wrong'
 
 def test_login_to_facebook():
-    assert project.login_to_facebook()
+    with pytest.raises(Exception):
+        project.login_to_facebook('test', 'test')
+    assert project.login_to_facebook('test', 'test') == 'something went wrong with logging in'
 
 def test_check_player_status():
     assert project.check_player_status() == 'checked'
