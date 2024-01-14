43b69e526f45fe3847cbde2f8d287e6a33fe62f2
Piotr Górny <84388562+nadziejapio@users.noreply.github.com>
2024-01-14 16:53:19 +0000

    Sun, Jan 14, 2024, 5:53 PM +01:00

--- final/project.py
+++ final/project.py
@@ -122,7 +122,7 @@ def send_facebook_message(message):
     message_box.send_keys(Keys.RETURN)
 
 def main():
-    nicks = get_nicks()
+    nicks = get_nicknames()
     for n in nicks:
         load_player(n)
     login_to_facebook(config.email, config.password )
