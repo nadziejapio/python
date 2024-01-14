656df67eeac0b28c90a4e236a60a3f8fa50e058b
Piotr Górny <84388562+nadziejapio@users.noreply.github.com>
2024-01-14 18:25:52 +0000

    Sun, Jan 14, 2024, 7:25 PM +01:00

--- final/project.py
+++ final/project.py
@@ -22,6 +22,7 @@ def get_nicknames():
             if check == 'y':
                 is_last = True
             elif check == 'n':
+                print(nicks)
                 return nicks
 
 players = []
