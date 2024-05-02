8c4a64ab8f1b56c98dff3da7799a8e57445e3fc7
Piotr Górny <84388562+nadziejapio@users.noreply.github.com>
2024-05-02 19:43:00 +0000

    Thu, May 2, 2024, 9:43 PM +02:00

--- final/project.py
+++ final/project.py
@@ -14,7 +14,9 @@ def get_nicknames():
     is_ended = False
     nicks = []
     while is_ended == False:
-        nicks.append(input('Enter nickname: '))
+        nick = input('Enter nickname: ')
+        tag = input('Enter tag: ')
+        nicks.append(f'{nick}#{tag}')
         is_last = False
         while is_last == False:
             check = input('Do you want to add more?(y/n) ')
@@ -27,10 +29,14 @@ def get_nicknames():
 players = []
 
 def load_player(players_Name):
-    url = f'https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{players_Name}'
+    players_nick = players_Name.split('#')[0]
+    players_tag = players_Name.split('#')[1]
+    url = f'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{players_nick}/{players_tag}'
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        players.append(player(players_Name, response.json()['id']))
+        players.append(player(players_Name, response.json()['puuid']))
+        print(f'{players_Name} loaded - {response.json()["puuid"]}')
+        plyer.notification.notify(title='LoL', message=f'{players_Name} loaded', app_name='Lol_Checker')
         return None
     else:
         raise Exception('Something is wrong')
@@ -40,11 +46,11 @@ def check_player_status():
         if p.current_game == 'justEnded':
             p.current_game = None
             continue
-        url = f'https://eun1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{p.id}'
+        url = f'https://eun1.api.riotgames.com/lol/spectator/v5/active-games/by-summoner/{p.id}'
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             if p.current_game == None:
-                plyer.notification.notify(title='LoL', message=f'{p.name} is playing {p.current_game}', app_name='Lol_Checker')
+                plyer.notification.notify(title='LoL', message=f'{p.name} is playing LoL', app_name='Lol_Checker')
             p.current_game = response.json()['gameId']
         elif response.status_code == 404:
             if p.current_game != None:
@@ -53,7 +59,7 @@ def check_player_status():
     return 'checked'
 
 def get_info(playerId, playerName, gameId):
-    url = f'https://europe.api.riotgames.com/lol/match/v5/matches/EUN1_{gameId}'
+    url = f'https://europe.api.riotgames.com/lol/match/v5/matches/{gameId}'
     response = requests.get(url, headers=headers)
     r2 = requests.get(f'https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/{playerId}', headers=headers)
     for i in r2.json():
@@ -92,7 +98,8 @@ def main():
         load_player(n)
     while True:
         check_player_status()
-        time.sleep(5)
+        print('checked')
+        time.sleep(15)
 
 if __name__ == '__main__':
     main()
